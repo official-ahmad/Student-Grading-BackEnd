fcc68650165474eb6d719533636ea579a7c28cff
Ahmad. <ahmadjuttboy516@gmail.com>
2026-05-22 20:40:04 +0500

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://student-grading-front-end.vercel.app/",
+      "https://student-grading-front-end.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
